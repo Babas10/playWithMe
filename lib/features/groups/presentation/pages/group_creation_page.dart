44f5555e3ab8593044d1206bbdcb05d001956d55
Etienne Dubois <etienne.dubois91@gmail.com>
2025-11-12 18:07:33 -0600

    feat(groups): integrate FriendSelectorWidget into GroupCreationPage
    - Add friend selection UI to group creation flow
    - Store selected friend IDs in component state
    - Pass selected friends to GroupBloc when creating group
    - Use GetIt to inject FriendRepository dependency
    - Position friend selector between description and info card

--- lib/features/groups/presentation/pages/group_creation_page.dart
+++ lib/features/groups/presentation/pages/group_creation_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:play_with_me/core/data/models/group_model.dart';
+import 'package:play_with_me/core/domain/repositories/friend_repository.dart';
 import 'package:play_with_me/core/presentation/bloc/group/group_bloc.dart';
 import 'package:play_with_me/core/presentation/bloc/group/group_event.dart';
 import 'package:play_with_me/core/presentation/bloc/group/group_state.dart';
 import 'package:play_with_me/features/auth/presentation/bloc/authentication/authentication_bloc.dart';
 import 'package:play_with_me/features/auth/presentation/bloc/authentication/authentication_state.dart';
+import '../widgets/friend_selector_widget.dart';
 
 class GroupCreationPage extends StatefulWidget {
   const GroupCreationPage({super.key});
@@ -18,6 +21,7 @@ class _GroupCreationPageState extends State<GroupCreationPage> {
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _descriptionController = TextEditingController();
+  Set<String> _selectedFriendIds = {};
 
   @override
   void dispose() {
@@ -56,7 +60,10 @@ class _GroupCreationPageState extends State<GroupCreationPage> {
       lastActivity: now, // Set initial activity to creation time
     );
 
-    context.read<GroupBloc>().add(CreateGroup(group: newGroup));
+    context.read<GroupBloc>().add(CreateGroup(
+          group: newGroup,
+          friendIdsToInvite: _selectedFriendIds.isNotEmpty ? _selectedFriendIds : null,
+        ));
   }
 
   @override
@@ -157,6 +164,19 @@ class _GroupCreationPageState extends State<GroupCreationPage> {
                         maxLines: 3,
                         maxLength: 200,
                         textCapitalization: TextCapitalization.sentences,
+                      ),
+                      const SizedBox(height: 24),
+
+                      // Friend selector
+                      FriendSelectorWidget(
+                        currentUserId: authState.user.uid,
+                        friendRepository: GetIt.instance<FriendRepository>(),
+                        onSelectionChanged: (selectedIds) {
+                          setState(() {
+                            _selectedFriendIds = selectedIds;
+                          });
+                        },
+                        initialSelection: _selectedFriendIds,
                       ),
                       const SizedBox(height: 24),
 
